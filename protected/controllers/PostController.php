4dbf6a3ae20bd8a3526a0a8454759fea7baba5cb
unknown <C:\Documents and Settings\Demon_id\Application Data\The Bat!>
2012-11-28 12:25:59 +0200

    lookup. status added

--- protected/controllers/PostController.php
+++ protected/controllers/PostController.php
@@ -26,23 +26,18 @@ class PostController extends Controller
 	 */
 	public function accessRules()
 	{
-		return array(
-			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('index','view'),
-				'users'=>array('*'),
-			),
-			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update'),
-				'users'=>array('@'),
-			),
-			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('admin','delete'),
-				'users'=>array('admin'),
-			),
-			array('deny',  // deny all users
-				'users'=>array('*'),
-			),
-		);
+                    return array(
+                       array('allow',  // allow all users to perform 'list' and 'show' actions
+                                'actions'=>array('index', 'view'),
+                                'users'=>array('*'),
+                        ),
+                        array('allow', // allow authenticated users to perform any action
+                                'users'=>array('*'),
+                        ),
+                        array('deny',  // deny all users
+                                'users'=>array('*'),
+                        ),
+                    );
 	}
 
 	/**
@@ -122,15 +117,8 @@ class PostController extends Controller
 	 */
 	public function actionIndex()
 	{
-<<<<<<< HEAD
-                
 		$dataProvider=new CActiveDataProvider('Post');
-                $dat=$dataProvider->getData();
-   		$this->render('index',array(
-=======
- 		$dataProvider=new CActiveDataProvider('Post');
                	$this->render('index',array(
->>>>>>> 21edf72ef98444e0bfe6e9b68113d89ab9a87257
 			'dataProvider'=>$dataProvider,
 		));
 	}
